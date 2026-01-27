1c7eab333836d5c8fe5a54a099089697f76c8fec
Jef van Zanten <j.van.zanten@gmail.com>
2026-01-27 11:18:24 +0100

    GithubLink height is now the same on the homepage and projectspage

--- src/components/GithubLink/GithubLink.tsx
+++ src/components/GithubLink/GithubLink.tsx
@@ -11,7 +11,7 @@ const GithubLink: Component<GithubLinkProps> = (props) => {
   return (
     <a class={styles.socialContainer} href={props.url} target="_blank">
       <img class={styles.logo} src={props.iconUrl} alt={`${props.name} logo`} />
-      <p class={styles.p}>{props.name}</p>
+      <p>{props.name}</p>
     </a>
   );
 };
